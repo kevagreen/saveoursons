2d2fac5da04dbbb877fbfecb22b3d75bcad36d7a
Shrunuti Singh <74618256+sunu09@users.noreply.github.com>
2021-03-15 17:51:26 -0500

    Delete saveoursons/src directory

--- saveoursons/src/main/java/com/tts/saveoursons/repository/CrimeCaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tts.saveoursons.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class CrimeCaseRepository extends CrudRepository<CrimeCase, Long>{
-}
-
