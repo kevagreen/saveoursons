c1494cb744fdf4e972c0979723f17627b941248c
Shrunuti Singh <74618256+sunu09@users.noreply.github.com>
2021-03-15 18:34:26 -0500

    Help button, help.html added

---
+++ README.md
@@ -0,0 +1,3 @@
+# saveoursons
+
+Capstone project collaboration building a full-stack application using a variety of front-end technologies. 
---
+++ src/main/java/com/tts/saveoursons/controller/MainController.java
@@ -0,0 +1,35 @@
+package com.tts.saveoursons.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value="/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping(value = "/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping(value = "/donate")
+    public String donate(){
+        return "donate";
+    }
+
+    @GetMapping(value = "/volunteer")
+    public String volunteer(){
+        return "volunteer";
+    }
+
+    @GetMapping(value = "/help")
+    public String contact(){
+        return "help";
+    }
+
+}
