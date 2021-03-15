e1359e525379c7fd14dd40da4868c2af51766e55
kevagreen <kevareynolds@gmail.com>
2021-03-15 13:31:25 -0600

    create interface for retrieving all volunteers or deleting volunteers

--- saveoursons/src/main/java/com/tts/saveoursons/service/VolunteerService.java
+++ saveoursons/src/main/java/com/tts/saveoursons/service/VolunteerService.java
@@ -1,4 +1,20 @@
 package com.tts.saveoursons.service;
 
-public interface VolunteerService {
+import com.tts.saveoursons.model.Volunteer;
+import com.tts.saveoursons.repository.VolunteerRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface VolunteerService{
+    Iterable<Volunteer> getAllVolunteers();
+    void addVolunteer(Volunteer volunteer);
+    void deleteById(Long id);
+
 }
