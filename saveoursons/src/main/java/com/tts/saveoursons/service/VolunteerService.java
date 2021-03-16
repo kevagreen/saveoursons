3f708e5b260d944ec42f4afcf6b97da883cb184d
kevagreen <kevareynolds@gmail.com>
2021-03-15 18:14:17 -0600

    add addVolunteer() method to VolunteerService

--- saveoursons/src/main/java/com/tts/saveoursons/service/VolunteerService.java
+++ saveoursons/src/main/java/com/tts/saveoursons/service/VolunteerService.java
@@ -1,20 +1,13 @@
 package com.tts.saveoursons.service;
 
 import com.tts.saveoursons.model.Volunteer;
-import com.tts.saveoursons.repository.VolunteerRepository;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public interface VolunteerService{
     Iterable<Volunteer> getAllVolunteers();
-    void addVolunteer(Volunteer volunteer);
+   void addVolunteer(Volunteer volunteer);
     void deleteById(Long id);
+    //Volunteer findByUsername(String email);
 
 }
