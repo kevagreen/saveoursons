ccaa9f78f23612f8d4a884016475dbb9cf23ab79
kevagreen <kevareynolds@gmail.com>
2021-03-15 18:18:11 -0600

    add BCryptPasswordEncode method

---
+++ saveoursons/src/main/java/com/tts/saveoursons/configuration/WebMvcConfiguration.java
@@ -0,0 +1,14 @@
+package com.tts.saveoursons.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebMvcConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
