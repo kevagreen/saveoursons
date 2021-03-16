9821ba9d842dcc9f1d6d6e06cb70c57957b85e9c
kevagreen <kevareynolds@gmail.com>
2021-03-15 18:16:07 -0600

    add findByUsername() method to VolunteerRepository

--- saveoursons/src/main/java/com/tts/saveoursons/repository/VolunteerRepository.java
+++ saveoursons/src/main/java/com/tts/saveoursons/repository/VolunteerRepository.java
@@ -1,8 +1,17 @@
 package com.tts.saveoursons.repository;
 
 
+import com.tts.saveoursons.model.Volunteer;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
-public class VolunteerRepository {
+public interface VolunteerRepository extends CrudRepository<Volunteer,Long> {
+
+    Optional<Volunteer> findByEmail(String email);
+    Volunteer findByUsername(String username);
+
 }
