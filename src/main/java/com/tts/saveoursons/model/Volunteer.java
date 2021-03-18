de7bf00fd7b0d2b5e9650739c759564696be2376
kevagreen <kevareynolds@gmail.com>
2021-03-18 12:59:27 -0600

    change fields to match volunteer form on volunteer.html

--- src/main/java/com/tts/saveoursons/model/Volunteer.java
+++ src/main/java/com/tts/saveoursons/model/Volunteer.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @Entity
 @EqualsAndHashCode
 @Table
-public class Volunteer implements UserDetails{
+public class Volunteer{
 
     @Id
     @GeneratedValue(strategy= GenerationType.SEQUENCE)
@@ -23,56 +23,24 @@ public class Volunteer implements UserDetails{
             allocationSize = 1
     )
     private Long id;
-    private String firstName;
-    private String lastName;
-    public String username;
-    private String password;
+    private String name;
     private String email;
     private Long phone;
+    private String address;
+    String volunteeringFor;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked = false;
-    private Boolean enabled = false;
 
 
 
-    public Volunteer(String firstName, String lastName, String password, String username, String email, Long phone, AppUserRole appUserRole) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
+    public Volunteer(String name, String lastName, String email, String address, Long phone, String volunteeringFor, AppUserRole appUserRole) {
+        this.name = name;
         this.email = email;
+        this.address = address;
         this.phone = phone;
+        this.volunteeringFor = volunteeringFor;
         this.appUserRole = appUserRole;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
 
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 }
