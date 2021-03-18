3d8935e9210629feebc14f8d71b879cd69131822
kevagreen <kevareynolds@gmail.com>
2021-03-18 12:57:19 -0600

    remove volunteer mapping to VolunteerController

--- src/main/java/com/tts/saveoursons/controller/MainController.java
+++ src/main/java/com/tts/saveoursons/controller/MainController.java
@@ -22,11 +22,6 @@ public class MainController {
         return "donate";
     }
 
-    @GetMapping(value = "/volunteer")
-    public String volunteer(){
-        return "volunteer";
-    }
-
     @GetMapping(value = "/help")
     public String contact(){
         return "help";
